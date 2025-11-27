17adb0258ef3ef41c8ae6867858b7a0464133e6e
Kolton Bloom <bloomkd46@icloud.com>
2025-11-27 15:03:29 -0800

    added home page title

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { TitleStrategy as NgTitleStrategy, provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { routes } from './app.routes';
+import { TitleStrategy } from './strategies/title.strategy';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), { provide: NgTitleStrategy, useClass: TitleStrategy }, provideClientHydration(withEventReplay())]
 };
