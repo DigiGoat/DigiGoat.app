d37883c15645a5614c2b23ffcec7e5aa3525df29
Kolton Bloom <bloomkd46@icloud.com>
2025-11-28 13:29:20 -0800

    enabled analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ImgDirective } from './directives/img/img.directive';
 import { TooltipDirective } from './directives/tooltip/tooltip.directive';
+import { AnalyticsComponent } from './features/analytics/analytics.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, NgOptimizedImage, ImgDirective, TooltipDirective],
+  imports: [RouterModule, NgOptimizedImage, ImgDirective, TooltipDirective, AnalyticsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/features/analytics/analytics.component.ts
@@ -0,0 +1,37 @@
+import { Component, ElementRef, inject, type OnInit } from '@angular/core';
+import { FirebaseService } from '../../services/firebase/firebase.service';
+import { PlatformService } from '../../services/platform/platform.service';
+
+@Component({
+  selector: 'analytics',
+  imports: [],
+  templateUrl: './analytics.component.html',
+  styleUrl: './analytics.component.scss'
+})
+export class AnalyticsComponent implements OnInit {
+  private firebaseService = inject(FirebaseService);
+  private el = inject(ElementRef<HTMLElement>);
+  private platformService = inject(PlatformService);
+  private document = this.el.nativeElement.ownerDocument;
+  ngOnInit(): void {
+    if (this.platformService.isServer) {
+      if (this.platformService.isDev) {
+        const script = this.document.createComment('Analytics disabled in development mode');
+        this.document.head.appendChild(script);
+      } else {
+        const clarity = 'udczxdpd2n';
+        if (clarity) {
+          const script = this.document.createElement('script');
+          script.innerHTML = `(function (c, l, a, r, i, t, y) { c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments); }; t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i; y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y); })(window, document, "clarity", "script", "${clarity}");`;
+          this.document.head.appendChild(script);
+        }
+      }
+    } else if (this.platformService.isBrowser) {
+      const color = this.document.documentElement.getAttribute('data-bs-theme') == 'light' ? 'Light' : 'Dark';
+      if ('clarity' in window) {
+        window.clarity('set', 'Color Scheme', color);
+      }
+      this.firebaseService.init();
+    }
+  }
+}
