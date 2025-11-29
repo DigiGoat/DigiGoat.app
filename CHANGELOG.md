6fe43c4aa31a61a9c68fed8080a125b47078c88b
Kolton Bloom <bloomkd46@icloud.com>
2025-11-28 19:12:03 -0800

    added an faq page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.1.0
+* Added an FAQ page!
+
 ## 1.0.2
 * The table on the homepage no longer freezes the first column when scrolling horizontally.
   * This was causing layout issues on smaller screens.
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent), title: 'Home' },
   { path: 'features', loadComponent: () => import('./pages/features/features.component').then(m => m.FeaturesComponent), title: 'Features' },
+  { path: 'faq', loadComponent: () => import('./pages/faq/faq.component').then(m => m.FaqComponent), title: 'FAQ' },
   { path: '**', loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent), title: 'Page Not Found' }
 ];
